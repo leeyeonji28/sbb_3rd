ec280bff9fffcaaab9e113594c50b1185dfeac64
leeyeonji28 <yeon102800@gmail.com>
2022-08-18 20:53:43 +0900

    컨트롤러

---
+++ src/main/java/com/mysite3/sbb_3rd/MainController.java
@@ -0,0 +1,14 @@
+package com.mysite3.sbb_3rd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @RequestMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        return "안녕하세요 sbb에 오신 것을 환영합니다.";
+    }
+}
