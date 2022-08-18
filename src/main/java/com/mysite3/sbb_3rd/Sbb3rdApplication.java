7495ba7416230f8259ae2559c9ee307a12d86614
leeyeonji28 <yeon102800@gmail.com>
2022-08-18 20:32:16 +0900

    셋팅

---
+++ src/main/java/com/mysite3/sbb_3rd/Sbb3rdApplication.java
@@ -0,0 +1,13 @@
+package com.mysite3.sbb_3rd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb3rdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbb3rdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mysite3/sbb_3rd/Sbb3rdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite3.sbb_3rd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb3rdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
