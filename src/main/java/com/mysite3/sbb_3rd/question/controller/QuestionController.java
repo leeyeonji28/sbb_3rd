9bb30ad0a6a0703863e0f3666b46a4aafaa63804
leeyeonji28 <yeon102800@gmail.com>
2022-08-20 22:57:33 +0900

    질문상세

--- src/main/java/com/mysite3/sbb_3rd/question/controller/QuestionController.java
+++ src/main/java/com/mysite3/sbb_3rd/question/controller/QuestionController.java
@@ -5,6 +5,7 @@ import com.mysite3.sbb_3rd.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,5 +23,12 @@ public class QuestionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @RequestMapping("/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id){
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/com/mysite3/sbb_3rd/question/service/QuestionService.java
+++ src/main/java/com/mysite3/sbb_3rd/question/service/QuestionService.java
@@ -1,9 +1,11 @@
 package com.mysite3.sbb_3rd.question.service;
 
+import com.mysite3.sbb_3rd.DataNotFoundException;
 import com.mysite3.sbb_3rd.question.dao.QuestionRepository;
 import com.mysite3.sbb_3rd.question.domain.Question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 import java.util.List;
 
@@ -15,5 +17,14 @@ public class QuestionService {
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()){
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question noe found");
+        }
     }
 }
