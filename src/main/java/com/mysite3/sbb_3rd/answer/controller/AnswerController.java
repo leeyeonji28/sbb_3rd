440c4b1d0e055cf5d2e6fbfe7aeb30e15ab32608
leeyeonji28 <yeon102800@gmail.com>
2022-08-22 00:09:59 +0900

    답변등록 수정

--- src/main/java/com/mysite3/sbb_3rd/answer/controller/AnswerController.java
+++ src/main/java/com/mysite3/sbb_3rd/answer/controller/AnswerController.java
@@ -18,7 +18,7 @@ public class AnswerController {
     private final QuestionService questionService;
     private final AnswerService answerService;
 
-    @PostMapping("/create/answer")
+    @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
         Question question = this.questionService.getQuestion(id);
         this.answerService.create(question, content);
